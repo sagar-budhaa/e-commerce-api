cc9801062fd351d5443369b32830fb34e4749a08
Sagar Budha <contact@sagarbudha.site>
2026-03-03 16:59:24 +0545

    remove route to get user details cuz it doesnt have security

--- routers/users.py
+++ routers/users.py
@@ -33,13 +33,6 @@ def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
     token = auth.create_access_token(data={"sub": str(db_user.id)})
     return {"access_token": token, "token_type": "bearer", "user": db_user}
 
-@router.get("/{user_id}", response_model=schemas.UserRead)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
 @router.get("/by-email/{email}", response_model=schemas.UserRead)
 def read_user_by_email(email: str, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email)
